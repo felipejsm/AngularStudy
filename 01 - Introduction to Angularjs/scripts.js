af8bfb6225790889642983ca4d04a55318060b70
felipejsm <felipejsm41@gmail.com>
2017-02-01 21:32:19 -0200

    Alterada versão do angular 1.5 -> 1.2 | adicionado ng-app

--- 01 - Introduction to Angularjs/scripts.js
+++ 01 - Introduction to Angularjs/scripts.js
@@ -1,5 +1,5 @@
 function PeopleController($scope) {
 	$scope.people = [{name : 'Jean Paul Sartre', phone : '3452345678', city : 'New York'},
 					 {name : 'Clarice Lispector', phone : '3452345674', city : 'Estônia'},
-					 {name : 'Arthur', phone : '3452345676', city : 'Bobaim Glamourosa'},];
+					 {name : 'Arthur', phone : '3452345676', city : 'Bobaim Glamourosa'}];
 }
